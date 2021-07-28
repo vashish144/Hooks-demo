944c8436e11ff5082e5f11845beaa906ec50b2cf
vikram <vashish144@gmail.com>
2021-07-28 16:31:30 +0530

    adding conditionally updating in countUsingUseEffect in useEffect method

--- src/components/UseEffectComponent/CounterUsingUseEffect.js
+++ src/components/UseEffectComponent/CounterUsingUseEffect.js
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from "react";
 function CounterUsingUseEffect() {
   const initialState = 0;
   const [count, setcount] = useState(initialState);
-
+  const [name, setName] = useState("");
   useEffect(() => {
     console.log("fired form useEffect");
     document.title = `you clicked ${count} time`;
-  });
+  }, [count]);
   return (
     <div>
+      <input
+        type="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setcount((preState) => preState + 1)}>
         click {count} time
       </button>
