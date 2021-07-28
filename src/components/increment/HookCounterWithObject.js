e23a4a7c40eed9c5ad5f287899527a83b9e71eea
vikram <vashish144@gmail.com>
2021-07-28 14:17:02 +0530

    adding spred opertator so that to prsist data to state

--- src/components/increment/HookCounterWithObject.js
+++ src/components/increment/HookCounterWithObject.js
@@ -4,6 +4,7 @@ function HookCounterWithObject() {
   const [name, setName] = useState({ firstName: "", lastName: "" });
   const lastNameHandler = (e) => {
     setName({
+      ...name,
       [e.target.name]: e.target.value,
     });
   };
@@ -13,12 +14,13 @@ function HookCounterWithObject() {
         type="text"
         name="firstName"
         value={name.firstName}
-        onChange={(e) => setName({ firstName: e.target.value })}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
       />
-      <br />
+
       <input type="text" name="lastName" onChange={lastNameHandler} />
       <h2>your First Name is: {name.firstName}</h2>
       <h2>your Second Name is: {name.lastName}</h2>
+      <h3>{JSON.stringify(name)}</h3>
     </form>
   );
 }
