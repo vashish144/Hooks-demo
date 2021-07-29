d56d8491171c1573100c88f174a0a663f6f376ff
vikram <vashish144@gmail.com>
2021-07-29 13:20:20 +0530

    adding concept of data feching on button click

--- src/components/DataFetching/DataFetchingHook.js
+++ src/components/DataFetching/DataFetchingHook.js
@@ -3,6 +3,7 @@ import axios from "axios";
 function DataFetchingHook() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
   useEffect(() => {
     axios
       .get(`http://jsonplaceholder.typicode.com/posts/${id}`)
@@ -13,10 +14,14 @@ function DataFetchingHook() {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={handleClick}>fetch post</button>
       <div>{post.title}</div>
       {/* <ul>
         {post.map((post) => (
